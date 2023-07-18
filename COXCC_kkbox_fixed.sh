be7787311f2a212ac464c4e6130c5393d8ef81bf
Lee Junhyun <junhyun@Leeui-MacBookPro.local>
2023-07-18 09:28:53 +0900

    Delete *.sh files

--- COXCC_kkbox_fixed.sh
+++
@@ -1,3 +0,0 @@
-for i in {0..33}; do 
-python COXCC_kkbox.py --use_BN --seed=$i --dataset=kkbox_v1 --wandb;
-done
--- DEEPSURV_kkbox_fixed.sh
+++
@@ -1,3 +0,0 @@
-for i in {0..32}; do 
-python DEEPSURV_kkbox.py --use_BN --seed=$i --dataset=kkbox_v1 --wandb;
-done
